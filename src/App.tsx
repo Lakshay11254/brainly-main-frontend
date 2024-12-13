1942edfa4d5777ab118c68c4a697f4161210d68a
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-12-12 20:37:06 -0500

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 // import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/dashboard";
+import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
-        <div>
-            {/* <Dashboard/> */}
-            <Signup/>
-        </div>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/signin" element={<Signin/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+        </Routes>
+        </BrowserRouter>
     )
 
 }
